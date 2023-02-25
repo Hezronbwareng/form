b639c72184419072e4a45be16e95f22b252d649d
Hezron Bett <bwarenghezron@gmail.com>
2023-02-26 01:11:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let loginform = document.getElementById('loginForm');
+
+loginForm.addEventListener("submit",(e) => {
+    e.preventDefault();
+
+
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+
+if (username.value == "" || password.value == "") {
+    alert("Ensure you input a value in both fields!");
+}
+else{
+    //perform opeeration with form input 
+    alert("This form has been successfully submitted!");
+    console.log('This form has a username of ${username.value} and password of ${password.value}');
+
+username.value = "";
+password.value = "";
+}});
+
